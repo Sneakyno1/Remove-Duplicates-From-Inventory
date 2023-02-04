f11df2dafb5407fcecfaa764e75e1d3a69f76f17
Andrew Ingles <mario.pacopal@gmail.com>
2023-02-04 13:39:52 -0500

    Added functionality to generate one csv file per category. Allowing for each category to have its own file.

--- Remove_Duplicates_From_Inventory.py
+++ Remove_Duplicates_From_Inventory.py
@@ -1,12 +1,13 @@
 import csv
 
+# these variables represent the corresponding index into the csv row
 ITEM_NAME = 1
 DEPARTMENT = 4
 CATEGORY = 5
 
 print("Starting")
 
-#Open the input csv in a content manageer
+#Open the input csv in a content manageer and read it into a list
 with open('2022workingcsv.csv','r', encoding='utf8') as inputCSV:
      CSVContent = csv.reader(inputCSV, delimiter = ',')
 
@@ -17,31 +18,43 @@ with open('2022workingcsv.csv','r', encoding='utf8') as inputCSV:
      print('Entries with \'zz\' removed')
 
 
-     #initializing a dictionary that will have the category name as the key
-     #the associated value will be a list of the items in that category
-     categoryDictionary = dict()
-     
-     for line in listWithNoZZ:
-         categoryDictionary[line[CATEGORY]]=list()
+#initializing a dictionary that will have the category name as the key
+#the associated value will be a list of the items in that category
+categoryDictionary = dict()
+
+for line in listWithNoZZ[1:]:
+    categoryDictionary[line[CATEGORY]]=list()
+
+for line in listWithNoZZ[1:]:
+    categoryDictionary[line[CATEGORY]].append(line[ITEM_NAME])
+    
+for key in categoryDictionary:
+    if key == 'general':
+        continue;
+    for item in categoryDictionary[key]:
+        if categoryDictionary['general'].count(item)>0:
+            print("found a duplicate item")
+            categoryDictionary['general'].remove(item)
+            
+
+for key in categoryDictionary:
+    print('Working on category: '+ key)
+    with open( key+'.csv', 'w', newline='', encoding='utf8') as categoryCSV:
+        output=csv.writer(categoryCSV, delimiter = ',')
+        output.writerow(listWithNoZZ[0])
+        for item in categoryDictionary[key]:
+            for line in listWithNoZZ[1:]:
+                if line[ITEM_NAME]==item:
+                    output.writerow(line)
+
+
+#open the output csv in another context manager
+with open('ZZClearedCSV.csv', 'w', newline='', encoding='utf8') as outputCSV:
+     output=csv.writer(outputCSV, delimiter = ',')
 
      for line in listWithNoZZ:
-         categoryDictionary[line[CATEGORY]].append(line[ITEM_NAME])
+          output.writerow(line)
 
-
-     #open the output csv in another context manager
-     with open('ZZClearedCSV.csv', 'w', newline='', encoding='utf8') as outputCSV:
-          output=csv.writer(outputCSV, delimiter = ',')
-
-          #We're doing this quick output.writereow() here so the headers will make it in 
-          #withoutbeing affected by  the following for loop 
-          output.writerow(listWithNoZZ[0])
-
-          #any row where the category isn't 'general' gets changed to 'general'
-          for line in listWithNoZZ[1:]:
-               if line[5] != 'general':
-                    line[5] = 'general'
-               output.writerow(line)
-
-               
+          
 
 print('done')
