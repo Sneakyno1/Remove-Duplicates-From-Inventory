34880355c4447db056661f8b5cd3613fe816901e
Andrew Ingles <mario.pacopal@gmail.com>
2023-02-15 12:02:33 -0500

    Added functionality to separate all seasonal categories into their own csv's, and all departments into their own csv's. But it won't add an entry into the department if that entry is in a seasonal category. Effectively making it so all seasonal items are separated from their departments.

--- Remove_Duplicates_From_Inventory.py
+++ Remove_Duplicates_From_Inventory.py
@@ -1,5 +1,6 @@
 import csv
 
+
 # these variables represent the corresponding index into the csv row
 ITEM_NAME = 1
 DEPARTMENT = 4
@@ -20,35 +21,58 @@ with open('2022workingcsv.csv','r', encoding='utf8') as inputCSV:
 
 #initializing a dictionary that will have the category name as the key
 #the associated value will be a list of the items in that category
-categoryDictionary = dict()
+seasonalDictionary = dict()
+
+#load dictionary with empty lists where the key is the category name
+#but only adds the category if it starts with the string seasonal
+for line in listWithNoZZ[1:]:
+    if line[CATEGORY][:8].lower()=='seasonal':
+        seasonalDictionary[line[CATEGORY]] = list()
+
+#add items to appropriate list based on category
+for line in listWithNoZZ[1:]:
+    if line[CATEGORY][:8].lower()=='seasonal':
+       seasonalDictionary[line[CATEGORY]].append(line[ITEM_NAME])
+
+#Initializing a dictionary that will hold lists like seasonalDicitonary
+departmentDictionary = dict()
+
+#adds entry into dictionary only if the category doesnt start with 'seasonal'
+for line in listWithNoZZ[1:]:
+    if line[CATEGORY][:8].lower()=='seasonal':
+        continue
+    departmentDictionary[line[DEPARTMENT]] = list()
 
 for line in listWithNoZZ[1:]:
-    categoryDictionary[line[CATEGORY]]=list()
+    if line[CATEGORY][:8].lower()=='seasonal':
+        continue
+    departmentDictionary[line[DEPARTMENT]].append(line[ITEM_NAME])
 
-for line in listWithNoZZ[1:]:
-    categoryDictionary[line[CATEGORY]].append(line[ITEM_NAME])
-    
-for key in categoryDictionary:
-    if key == 'general':
-        continue;
-    for item in categoryDictionary[key]:
-        if categoryDictionary['general'].count(item)>0:
-            print("found a duplicate item")
-            categoryDictionary['general'].remove(item)
-            
 
-for key in categoryDictionary:
+#Makes one csv per category
+for key in seasonalDictionary:
     print('Working on category: '+ key)
     with open( key+'.csv', 'w', newline='', encoding='utf8') as categoryCSV:
         output=csv.writer(categoryCSV, delimiter = ',')
         output.writerow(listWithNoZZ[0])
-        for item in categoryDictionary[key]:
+        for item in seasonalDictionary[key]:
+            for line in listWithNoZZ[1:]:
+                if line[ITEM_NAME]==item:
+                    output.writerow(line)
+
+#Makes one csv per department
+for key in departmentDictionary:
+    print('Working on department: '+ key)
+    with open( key+'.csv', 'w', newline='', encoding='utf8') as departmentCSV:
+        output=csv.writer(departmentCSV, delimiter = ',')
+        output.writerow(listWithNoZZ[0])
+        for item in departmentDictionary[key]:
             for line in listWithNoZZ[1:]:
                 if line[ITEM_NAME]==item:
                     output.writerow(line)
 
 
-#open the output csv in another context manager
+#write the total csv without 'zz' including all departments and categories
 with open('ZZClearedCSV.csv', 'w', newline='', encoding='utf8') as outputCSV:
      output=csv.writer(outputCSV, delimiter = ',')
 
